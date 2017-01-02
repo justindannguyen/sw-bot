0313b50dc6e8fdb027beb2e67127d5cc86b3807b
Justin Nguyen <tuan3.nguyen@gmail.com>
2017-01-02 23:38:56 +0700

    Add main class

---
+++ src/main/java/com/justin/swbot/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (C) 2017, Justin Nguyen
+ */
+
+package com.justin.swbot;
+
+/**
+ * @author tuan3.nguyen@gmail.com
+ */
+public class Main {
+	public static void main(String[] args) {
+
+	}
+}
