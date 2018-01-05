0c8d0b41d946d7883e59ccf017a91f7aeb6a8236
Akivamu <akivamu@gmail.com>
2018-01-05 09:44:17 +0700

    Add screen rotation (manually hard code)

--- sw-bot-desktop/src/main/java/com/justin/swbot/util/PcCommandUtil.java
+++ sw-bot-desktop/src/main/java/com/justin/swbot/util/PcCommandUtil.java
@@ -5,6 +5,10 @@ package com.justin.swbot.util;
 
 import com.justin.swbot.game.GameStatus;
 
+import javax.imageio.ImageIO;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -16,6 +20,9 @@ import java.util.Arrays;
  * @author tuan3.nguyen@gmail.com
  */
 public class PcCommandUtil implements CommandUtil {
+  // TODO: temporary fix for portrait screen orientation
+  private static final int quadrants = 0;
+
   @Override
   public String capturePhoneScreen() {
     final File screenshotFile =
@@ -23,7 +30,48 @@ public class PcCommandUtil implements CommandUtil {
     runCmd("adb", "shell", "screencap", "-p", "/mnt/sdcard/output.png");
     runCmd("adb", "pull", "/mnt/sdcard/output.png", screenshotFile.getAbsolutePath());
     runCmd("adb", "shell", "rm", "/mnt/sdcard/output.png");
+
+    if (quadrants != 0) {
+      try {
+        rotateImage(screenshotFile.getAbsolutePath(), quadrants);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
     return screenshotFile.exists() ? screenshotFile.getAbsolutePath() : null;
+  }
+
+  private void rotateImage(String path, int quadrants) throws IOException {
+    BufferedImage image = ImageIO.read(new File(path));
+
+    int w0 = image.getWidth();
+    int h0 = image.getHeight();
+    int w1 = w0;
+    int h1 = h0;
+    int centerX = w0 / 2;
+    int centerY = h0 / 2;
+
+    if (quadrants % 2 == 1) {
+      w1 = h0;
+      h1 = w0;
+    }
+
+    if (quadrants % 4 == 1) {
+      centerX = h0 / 2;
+      centerY = h0 / 2;
+    } else if (quadrants % 4 == 3) {
+      centerX = w0 / 2;
+      centerY = w0 / 2;
+    }
+
+    AffineTransform affineTransform = new AffineTransform();
+    affineTransform.setToQuadrantRotation(quadrants, centerX, centerY);
+    AffineTransformOp opRotated = new AffineTransformOp(affineTransform, AffineTransformOp.TYPE_BILINEAR);
+    BufferedImage transformedImage = new BufferedImage(w1, h1, image.getType());
+    transformedImage = opRotated.filter(image, transformedImage);
+
+    ImageIO.write(transformedImage, "PNG", new File(path));
   }
 
   @Override
